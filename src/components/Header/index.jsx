fd4059332b33580413bb69a796106af3d71fd7b0
Gabriel RD <gabrielrd.dev@gmail.com>
2024-08-29 20:25:09 -0300

    updateteste

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,9 +7,9 @@ export function Header() {
 <nav>
     <div class='logo'>Treine.me</div>
     <ul class='navbar'> 
-        <li id='link'><a href="">Home</a></li>
-        <li id='link'><a href="">Sobre</a></li>
-        <li id='link'><a href="">Treinar</a></li>
+        <li><a href="#" class='navHome'>Home</a></li>
+        <li><a href="#" class='navSobre'>Sobre</a></li>
+        <li><a href="#" class='navTreinar'>Treinar</a></li>
     </ul>
     </nav>
     </>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import enderecoimg from '../../../public/img/image01.jpg'
+import enderecoimg1 from '../../../public/img/image.svg'
 import  {Footer}  from '../../components/Footer'
 import  {Header}  from '../../components/Header'
 import './styles.css'
@@ -14,7 +15,14 @@ export function Home() {
     
       <img id='testeimg' src={enderecoimg} alt="" />
 
-      <h1 id='h1home'> pagina principal </h1>
+      <h1 id='h1home'>Treinos <span>exclusivos</span> <br /> para você!</h1>
+
+      <p>Nós criamos treinos <span class='cinza1'>exclusivos e únicos para você.</span> <br />
+      Invista no seu corpo e  <span class='cinza1'>tenha muito mais performance </span> e <br /> qualidade de vida.</p>
+      <div class='box'>
+        <a href="#" class='linkBox'>Comece já</a>
+            <img class='logowhats' src={enderecoimg1} alt="" />
+      </div>
 
     </div>
 
